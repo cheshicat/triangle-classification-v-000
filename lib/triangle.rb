f1719687c966cb89299a89703b277e83bace75a7
Allison Howard <alliemhow@gmail.com>
2018-11-26 20:53:10 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -11,7 +11,7 @@ class Triangle
     if @a + @b <= @c || @a + @c <= @b || @b + @c <= @a || @a == 0 || @b == 0 || @c == 0
       raise TriangleError
     elsif @a == @b && @b == @c
-      retun :equilateral
+      return :equilateral
     elsif @a == @b || @b == @c || @a == @c
       return :isosceles
 
