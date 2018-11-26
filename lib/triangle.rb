a88e4d2025f6634b0efae8052a67842efb14d7c5
Allison Howard <alliemhow@gmail.com>
2018-11-26 20:45:19 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -3,21 +3,8 @@ class Triangle
 
   def initialize (a, b, c)
     @a = a
-    @b = b 
+    @b = b
     @c = c
   end
 
 end
-def initialize(a, b, c) 
-
-
-   @a = a 
-
-
-   @b = b 
-
-
-   @c = c 
-
-
-end 
